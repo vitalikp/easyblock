c5588d91338a4caf36dc06583a6b3933a16e8da0
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2019-08-07 02:47:31 +0300

    * content: add “ContentObserver” class;

--- content/content.js
+++ content/content.js
@@ -16,6 +16,88 @@ const ContentAPI =
 	}
 };
 
+function ContentObserver()
+{
+	this.dom = null;
+}
+
+ContentObserver.prototype =
+{
+	clear: function()
+	{
+		this.dom = null;
+	},
+
+	onFind: function(doc, dom)
+	{
+		if (!doc || !dom || !dom.length)
+			return;
+
+		this.dom = dom;
+		this.filterNode(doc.body);
+	},
+
+	onDomLoad: function(event, content)
+	{
+		let doc, loc;
+
+		if (!event || !content)
+			return;
+
+		doc = event.originalTarget;
+		if (!doc || doc.nodeType != doc.DOCUMENT_NODE)
+			return;
+
+		loc = doc.location;
+		if (loc.protocol != "https:" && loc.protocol != "http:")
+			return;
+
+		if (this.dom)
+			this.filterNode(doc.body);
+		else
+			content.findDom(loc.hostname, (dom) => this.onFind(doc, dom));
+	},
+
+	filterNode: function(node)
+	{
+		let nodes, sel, i;
+
+		if (!this.dom || !node || !node.parentElement || node.nodeType != node.ELEMENT_NODE)
+			return;
+
+		node = node.parentElement;
+
+		i = 0;
+		while (i < this.dom.length)
+		{
+			sel = this.dom[i++];
+			if (!sel)
+				continue;
+
+			nodes = node.querySelectorAll(sel);
+			this.filterNodes(nodes);
+			nodes = null;
+		}
+	},
+
+	filterNodes: function(nodes)
+	{
+		let i, node;
+
+		if (!nodes || !nodes.length)
+			return;
+
+		i = 0;
+		while (i < nodes.length)
+		{
+			node = nodes[i++];
+			if (node && node.parentElement)
+				node.parentElement.removeChild(node);
+			node = null;
+		}
+	}
+};
+
 const ContentFilter =
 {
 	filter: null,
