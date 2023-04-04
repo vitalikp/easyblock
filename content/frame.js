61c40a8093bea6ac1143e9657e0116d74513620f
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2023-04-04 20:22:51 +0300

    * javascript: add “frame.js” script;

---
+++ content/frame.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const Cu = Components.utils;
+
+
+// import
+Cu.import("chrome://easyblock/content/content.js");
+
+
+const ContentAPI =
+{
+	regEvent(event, handler)
+	{
+		addMessageListener(event, handler);
+	},
+
+	unregEvent(event, handler)
+	{
+		removeMessageListener(event, handler);
+	},
+
+	sendSyncEvent(event, data)
+	{
+		return sendSyncMessage(event, data);
+	}
+};
+
+
+let handler;
+
+handler = new SiteHandler(ContentAPI);
+addEventListener("DOMContentLoaded", handler);
+addEventListener('unload', handler);
