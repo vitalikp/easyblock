6e4cb2cf05946c67ca866cd26f19442afe3af36d
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2024-02-02 23:15:25 +0200

    * frame: remove “ContentAPI” helper object;

--- content/frame.js
+++ content/frame.js
@@ -9,25 +9,6 @@ Cu.import("chrome://easyblock/content/content.js");
 const global = this;
 
 
-const ContentAPI =
-{
-	regEvent(event, handler)
-	{
-		addMessageListener(event, handler);
-	},
-
-	unregEvent(event, handler)
-	{
-		removeMessageListener(event, handler);
-	},
-
-	sendSyncEvent(event, data)
-	{
-		return sendSyncMessage(event, data);
-	}
-};
-
-
 let handler;
 
 handler = new SiteHandler(global);
